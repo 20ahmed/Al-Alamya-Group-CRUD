4fd43126e862b5461898bf9c8b574554778755d5
20ahmed <shabana7052@yahoo.com>
2023-03-04 15:30:35 +0200

    ay 7aga

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var allProducts; // lazm yt3rf bra
+var selectedProduct = null;
+const btn = document.getElementById("btn");
 
 if (localStorage.getItem("productsData") == null) {
   // da m3nah n l ragel md5l4 7aga fe l localstorage lsa
@@ -15,33 +17,40 @@ function addProduct() {
   let productTitle = document.getElementById("productTitleInp").value;
   let productCode = document.getElementById("productCodeInp").value;
   let productDescription = document.getElementById("productDescInp").value;
-
   let dashPlace = productTitle.search("-");
   let productBrand = productTitle.slice(0, dashPlace);
   let productModel = productTitle.slice(dashPlace + 1, productTitle.length); // aw bdl l productName.length mmkn aktb -1
 
   let products = {
     Name: productName,
-    price: productPrice,
-    category: productCategory,
-    brand: productBrand,
-    model: productModel,
-    code: productCode,
-    desc: productDescription,
+    price: productPrice || "2000",
+    category: productCategory || "غير مصنف",
+    brand: productBrand || "لايوجد",
+    model: productModel || "لا يوجد",
+    code: productCode || "ليس له كود",
+    desc: productDescription || "ليس له وصف",
   };
 
-  allProducts.push(products);
+  let inputField = document.getElementsByClassName("form-control");
+
+  const validInputs = Array.from(inputField).filter(
+    (input) => input.value !== ""
+  );
+  if (validInputs.length === 0) {
+    alert("please fill the fields");
+  } else {
+    allProducts.push(products);
+  }
 
   localStorage.setItem("productsData", JSON.stringify(allProducts)); //34an arg3 tany l data ly gyaly ly object
   displayProducts();
 
-  console.log(allProducts);
+  // console.log(allProducts);
 }
-
-const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function handleClick(event) {
   event.preventDefault();
+  // console.log(inputField);
 
   const inputs = document.querySelectorAll(
     "#productNameInp, #productPriceInp ,#productCatInp ,#productTitleInp ,#productCodeInp ,#productDescInp "
@@ -51,35 +60,39 @@ btn.addEventListener("click", function handleClick(event) {
     input.value = "";
   });
 }); // d 34an afdy l inputs b3d ma a3ml add product
+
 function displayProducts() {
-  var temp = "";
+  if (allProducts.length === 0) {
+    document.getElementById("emptyDiv").style.display = "block";
+    document.getElementById("product_container").style.display = "none";
+    // allProducts = [];
+  } else {
+    var temp = "";
 
-  for (let i = 0; i < allProducts.length; i++) {
-    temp += `  <div class="col-md-3">
-    <div class="product">
-        <img src="imges/rachit-tank-2cFZ_FB08UM-unsplash.jpg" class="img-fluid" alt="">
-        <h3>
-           ${allProducts[i].Name} <span class="badge badge-info"> ${allProducts[i].brand}</span>
-        </h3>
-        <p> ${allProducts[i].desc}</p>
-        <div class="price p-2"> ${allProducts[i].price}</div>
-        <button onclick="deleteProduct(${i})" class= "btn-outline-danger btn  mb-2">delete</button> 
-        <button onclick="updateProduct()" class= "btn-outline-warning btn  mb-2">update</button>
+    for (let i = 0; i < allProducts.length; i++) {
+      temp += `  <div class="col-md-3">
+  <div class="product">
+      <img src="imges/rachit-tank-2cFZ_FB08UM-unsplash.jpg" class="img-fluid" alt="">
+      <h3>
+         ${allProducts[i].Name} <span class="badge badge-info"> ${allProducts[i].brand}</span>
+      </h3>
+      <p> ${allProducts[i].desc}</p>
+      <p> ${allProducts[i].code}</p> <p> ${allProducts[i].category}</p>
+      <div class="price p-2"> ${allProducts[i].price}</div>
+      <button onclick="deleteProduct(${i})" class= "btn-outline-danger btn  mb-2">delete</button> 
+      <button onclick="onEdit(${i})" class= "btn-outline-warning btn  mb-2">edit</button>
 
-    </div>
+  </div>
 </div>
 `;
 
-    //hdelo l i fe l delete 34an a2olo lma ados 3la l zorar nfz l function deleteProduct w ab3t rkm l montg ly atdas 3leh
+      //hdelo l i fe l delete 34an a2olo lma ados 3la l zorar nfz l function deleteProduct w ab3t rkm l montg ly atdas 3leh
+      document.getElementById("emptyDiv").style.display = "none";
+      document.getElementById("product_container").style.display = "flex";
 
-    document.getElementById("product_container").innerHTML = temp;
-  }
-  if (temp == []) {
-    document.getElementById("emptyDiv").style.display = "block";
-
-    console.log(allProducts);
-  } else {
-    document.getElementById("emptyDiv").style.display = "none";
+      document.getElementById("product_container").innerHTML = temp;
+      onEdit();
+    }
   }
 }
 function searchProduct(term) {
@@ -115,15 +128,35 @@ function deleteProduct(indx) {
   displayProducts();
 }
 
-function validateForm(userName) {
-  var userNameRegex = /^[\u0621-\u064A\u0660-\u0669 ]+$/;
-  if (userNameRegex.test(userName) == false) {
-    // inputs.forEach((input) => {
-    //   input.value == "";
-    // })
-    //method test bst5dmha m3 l regular expression bs
-    document.getElementById("btn").disabled = "true";
-  } else {
-    document.getElementById("btn").removeAttribute("disabled");
+// function validateForm(userName) {
+//   var userNameRegex = /^[\u0621-\u064A\u0660-\u0669 ]+$/;
+
+//   if (userNameRegex.test(userName) == false) {
+//     // inputs.forEach((input) => {
+//     //   input.value == "";
+//     // })
+//     //method test bst5dmha m3 l regular expression bs
+//     document.getElementById("btn").disabled = "true";
+//   } else {
+//     document.getElementById("btn").removeAttribute("disabled");
+//   }
+// }
+
+function onEdit() {
+  for (let i = 0; i < allProducts.length; i++) {
+    var selectedProduct = allProducts[i];
+
+    console.log(selectedProduct);
   }
+  document.getElementById("productNameInp").value = selectedProduct.name;
+  console.log(selectedProduct.name);
+  document.getElementById("productPriceInp").value = selectedProduct.price;
+  console.log(selectedProduct.price);
+
+  // document.getElementById("productCatInp").value=
+  // document.getElementById("productTitleInp").value;
+  document.getElementById("productCodeInp").value = selectedProduct.code;
+  // document.getElementById("productDescInp").value;
 }
+
+function updateProduct(selectedProduct) {}
